14e3180148c2d20cf8fd40b1398c6be5a848cacd
Ivan <iva.drakon.nov@gmail.com>
2020-08-22 19:08:27 +0200

    Profile page new stability version

--- profile/src/main.js
+++ profile/src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-
+import './assets/css/style.less'
+import './assets/css/tablet.css'
+import './assets/css/mobile.css'
+import './assets/css/print.css'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount('#app')
