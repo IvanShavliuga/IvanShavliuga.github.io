3e76c2c2f12be8cd6f0189fcca988469f02dc9f9
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-09-03 09:13:35 +0300

    crios added images

---
+++ profile/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderProf
+      name="Ivan Ivanov Shavliuga"
+      geo="Novopolotsk"
+      email="iv2@gtb.com"
+      prof="js developer"
+      phone="37500"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderProf from '@/components/HeaderProf.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    HeaderProf
+  }
+}
+</script>
