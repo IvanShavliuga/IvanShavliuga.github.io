2a769b150c416cf34d6996c74c7a2670d9d7b2eb
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2021-02-11 12:52:41 +0000

    archive updated

--- archive/projects/Examples/DBTasks/BkQuery/amountdu.sql
+++
@@ -1,4 +0,0 @@
-select Company, Sum(ItemsTotal) - Sum(AmountPaid) as AmountDue
-  from customer, orders
-  where Customer.CustNo = Orders.CustNo
-  group by Company
--- archive/projects/Examples/DBTasks/BkQuery/ineffcnt.sql
+++
@@ -1,3 +0,0 @@
-select EMP_NO, Avg(Salary) as Salary
-  from employee, employee, employee
-  group by EMP_NO
