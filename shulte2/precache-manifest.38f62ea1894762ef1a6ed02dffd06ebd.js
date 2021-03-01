66287c52e19b0308d8a7d7dc14d372717defaf4f
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2021-03-01 19:45:23 +0300

    shulte build setting

--- shulte2/precache-manifest.38f62ea1894762ef1a6ed02dffd06ebd.js
+++ shulte2/precache-manifest.38f62ea1894762ef1a6ed02dffd06ebd.js
@@ -1,54 +1,54 @@
 self.__precacheManifest = [
   {
     "revision": "169832d9d33fa5d4ebea",
-    "url": "/js/about.ccbf5d00.js"
+    "url": "./js/about.ccbf5d00.js"
   },
   {
     "revision": "dea5a93bba5e3844b25b",
-    "url": "/css/app.13628693.css"
+    "url": "./css/app.13628693.css"
   },
   {
     "revision": "dea5a93bba5e3844b25b",
-    "url": "/js/app.421252ad.js"
+    "url": "./js/app.421252ad.js"
   },
   {
     "revision": "37594ca231326478a865",
-    "url": "/js/chunk-vendors.7f103b48.js"
+    "url": "./js/chunk-vendors.7f103b48.js"
   },
   {
     "revision": "c705da5697bc59a3bd0ba086921b1d51",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "a68050aa316a9a78856cbd1ac8dafa10",
-    "url": "/img/icons-old/fuji-mountain-128x128.png"
+    "url": "./img/icons-old/fuji-mountain-128x128.png"
   },
   {
     "revision": "0a5afac7cb039c2cc0552c44ab6077f4",
-    "url": "/img/icons-old/fuji-mountain-16x16.png"
+    "url": "./img/icons-old/fuji-mountain-16x16.png"
   },
   {
     "revision": "e7b06aca80396527699c7da03f378815",
-    "url": "/img/icons-old/fuji-mountain-32x32.png"
+    "url": "./img/icons-old/fuji-mountain-32x32.png"
   },
   {
     "revision": "a6c149ae650a8a07e5c6cb4bae8e4964",
-    "url": "/img/icons-old/fuji-mountain-512x512.png"
+    "url": "./img/icons-old/fuji-mountain-512x512.png"
   },
   {
     "revision": "b89032a4a5a1879f30ba05a13947f26f",
-    "url": "/img/icons-old/msapplication-icon-144x144.png"
+    "url": "./img/icons-old/msapplication-icon-144x144.png"
   },
   {
     "revision": "058a3335d15a3eb84e7ae3707ba09620",
-    "url": "/img/icons-old/mstile-150x150.png"
+    "url": "./img/icons-old/mstile-150x150.png"
   },
   {
     "revision": "f78c0251d6ddd56ee219a1830ded71b4",
-    "url": "/img/icons-old/safari-pinned-tab.svg"
+    "url": "./img/icons-old/safari-pinned-tab.svg"
   },
   {
     "revision": "333fe3e10f3cab8768e778d581c4b3c3",
-    "url": "/iv2.ico"
+    "url": "./iv2.ico"
   }
 ];
