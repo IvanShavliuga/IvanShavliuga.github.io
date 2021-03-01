0961b9a68de258137c0f78fc3a981cec344187d7
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2021-03-01 20:38:14 +0300

    ааа

--- shulte2/service-worker.js
+++ shulte2/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://ivanshavliuga.github.io/shulte2/precache-manifest.38f62ea1894762ef1a6ed02dffd06ebd.js"
+ "https://ivanshavliuga.github.io/shulte2/precache-manifest.38f62ea1894762ef1a6ed02dffd06ebd.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "iv2shulte"});
