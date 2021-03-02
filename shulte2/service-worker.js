2c1c7e5d972c9e40925a5250dc6c890f05d13d60
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2021-03-02 16:18:18 +0300

    Update service-worker.js

--- shulte2/service-worker.js
+++ shulte2/service-worker.js
@@ -1,8 +1,1 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-importScripts("https://ivanshavliuga.github.io/shulte2/precache-manifest.38f62ea1894762ef1a6ed02dffd06ebd.js");
-workbox.core.setCacheNameDetails({prefix: "iv2shulte"});
-workbox.skipWaiting();
-workbox.clientsClaim();
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");importScripts("https://ivanshavliuga.github.io/shulte2/precache-manifest.38f62ea1894762ef1a6ed02dffd06ebd.js");workbox.core.setCacheNameDetails({prefix: "iv2shulte"});workbox.skipWaiting();workbox.clientsClaim();self.__precacheManifest = [].concat(self.__precacheManifest || []);workbox.precaching.suppressWarnings();workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
