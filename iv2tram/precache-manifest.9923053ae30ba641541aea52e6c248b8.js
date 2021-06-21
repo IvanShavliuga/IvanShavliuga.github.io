80e9aedc52bb8d08e71541e21906e94107815ac5
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2021-06-21 18:05:55 +0300

    iv2tram test

---
+++ iv2tram/precache-manifest.9923053ae30ba641541aea52e6c248b8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a33a41e411b0aeb6f3b",
+    "url": "js/about.9f8037cf.js"
+  },
+  {
+    "revision": "339e935cddebe168e1d1",
+    "url": "css/app.3462139b.css"
+  },
+  {
+    "revision": "339e935cddebe168e1d1",
+    "url": "js/app.289d3d50.js"
+  },
+  {
+    "revision": "77f7378a489e7b4db5bc",
+    "url": "js/chunk-vendors.d35cb52e.js"
+  },
+  {
+    "revision": "81d5b6acb87eff6d80d0e0f626358416",
+    "url": "index.html"
+  },
+  {
+    "revision": "c5b3b84645b2bd6a986ca7bd05bf1e9a",
+    "url": "digital.ttf"
+  },
+  {
+    "revision": "f821186041b6460780efeb76341eefe6",
+    "url": "img/tram.svg"
+  },
+  {
+    "revision": "9be6c95134431cd7aa9319e7b5a274a8",
+    "url": "iv2.ico"
+  },
+  {
+    "revision": "df3214617fe291cc7e68dd54d091f399",
+    "url": "iv2tram.ico"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "6c470f5a05c48ed6bd7680629df6ad3d",
+    "url": "style.css"
+  }
+];
