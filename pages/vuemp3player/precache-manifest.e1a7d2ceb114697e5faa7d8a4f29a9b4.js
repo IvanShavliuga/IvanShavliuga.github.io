d3fe0642ec40e93393c6f9f7d8b94401c2869242
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2021-06-24 17:38:14 +0300

    restore player link

---
+++ pages/vuemp3player/precache-manifest.e1a7d2ceb114697e5faa7d8a4f29a9b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a33a41e411b0aeb6f3b",
+    "url": "https://ivanshavliuga.github.io/pages/vuemp3player/js/about.9f8037cf.js"
+  },
+  {
+    "revision": "e9cb6b98c5365bbcc783",
+    "url": "https://ivanshavliuga.github.io/pages/vuemp3player/css/app.0b42a60d.css"
+  },
+  {
+    "revision": "e9cb6b98c5365bbcc783",
+    "url": "https://ivanshavliuga.github.io/pages/vuemp3player/js/app.fa1ffe30.js"
+  },
+  {
+    "revision": "a76557c1134c0ecb309c",
+    "url": "https://ivanshavliuga.github.io/pages/vuemp3player/js/chunk-vendors.408a0b51.js"
+  },
+  {
+    "revision": "65ef70b1f06658902047344468dd8cb9",
+    "url": "https://ivanshavliuga.github.io/pages/vuemp3player/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "https://ivanshavliuga.github.io/pages/vuemp3player/robots.txt"
+  },
+  {
+    "revision": "9be6c95134431cd7aa9319e7b5a274a8",
+    "url": "https://ivanshavliuga.github.io/pages/vuemp3player/iv2.ico"
+  }
+];
